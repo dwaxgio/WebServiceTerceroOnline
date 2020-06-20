bf5df950af8b04ebede9ae6363cafe7badce144c
edward_gio <edward_gio@hotmail.com>
2020-06-20 15:21:29 -0500

    // Web service tomado de: http://www.dneonline.com/calculator.asmx

---
+++ Index.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="WebServiceTerceroOnline.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TxtBox1" runat="server"></asp:TextBox>
+            <asp:TextBox ID="TxtBox2" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Suma" OnClick="Button1_Click"  />
+            <asp:Button ID="Button2" runat="server" Text="Resta" OnClick="Button2_Click"  />
+            <asp:Button ID="Button3" runat="server" Text="Multiplicación" OnClick="Button3_Click"  />
+            <asp:Button ID="Button4" runat="server" Text="División" OnClick="Button4_Click"  />
+            <asp:Label ID="Label1" runat="server" Text="Resultado"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebServiceTerceroOnline
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        // Web service tomado de: http://www.dneonline.com/calculator.asmx
+        // 2. Instancia para acceso global
+        WebServiceOnline.CalculatorSoapClient ws = new WebServiceOnline.CalculatorSoapClient();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // suma
+            Label1.Text = ws.Add(Convert.ToInt32(TxtBox1.Text), Convert.ToInt32(TxtBox2.Text)).ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            // Resta
+            Label1.Text = ws.Subtract(Convert.ToInt32(TxtBox1.Text), Convert.ToInt32(TxtBox2.Text)).ToString();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            // Multiplicacion
+            Label1.Text = ws.Multiply(Convert.ToInt32(TxtBox1.Text), Convert.ToInt32(TxtBox2.Text)).ToString();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            // División
+            Label1.Text = ws.Divide(Convert.ToInt32(TxtBox1.Text), Convert.ToInt32(TxtBox2.Text)).ToString();
+        }
+    }
+}
